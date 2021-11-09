73b490b9e58505d6a4d99a7d3cd27ea7ebcf83af
kingbaddo <wizzyblaq94@gmail.com>
2021-11-09 10:17:41 +0100

    app files and imports

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import JobPanel from './JobPanel';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="container">
+       <div className="header"></div>
+       <div className="content">
+         <JobPanel/>
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
